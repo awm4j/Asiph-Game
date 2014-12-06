85db07c49cb8522c26e697e88a9a82a458b16ae3
Nate Hagan <hagannr1@gcc.edu>
2014-12-06 14:29:42 -0800

    Added initial Game file

---
+++ js/Game/Game1.js
@@ -0,0 +1,27 @@
+/**
+ * Created by Nate on 12/6/2014.
+ */
+
+
+var game = new Phaser.Game(800, 600, Phaser.CANVAS, '{preload: Preload, create: Create, update: Update, render: Render }');
+
+function Preload()
+{
+    
+}
+
+
+function Create()
+{
+
+}
+
+function Update()
+{
+
+}
+
+function Render()
+{
+
+}

84f6f0c99ed3a74de0b0134f4e22d4461d24e22b
awm4j <awebmaster4jesus@gmail.com>
2014-12-07 01:58:48 -0500

    updating assets

--- js/Game/Game1.js
+++ js/Game/Game1.js
@@ -1,22 +1,30 @@
+/**
+ * Created by Nate on 12/6/2014.
+ * Modified by AwM4J 12/6/14.
+ */
+
 var game;
 
 var gameWidth = 600;
-var kodingWidth = 400;
+var kodingWidth = 400; // ;)
 var totalWidth = gameWidth + kodingWidth;
 var totalHeight = 600;
 
 var GAME_SPEED = 1;
 var PLAYER_SPEED = 4;
 
-///The main game class
+//The main game class
 Game1 = function() {
     game = new Phaser.Game(totalWidth, totalHeight, Phaser.CANVAS, 'phaser-game', {preload: Preload, create: Create, update: Update, render: Render });
-
-    ///Use to load the game assets
+	this.player;
+	this.cursors;
+    this.controlManager;
 var bot;
+    //Called before the game is started
+    //Use to load the game assets
     function Preload() {
 		game.load.image('player','assets/player.png');
-		game.load.atlasJSONHash('bot', 'assets/mainChar/walkUp/spriteSheet.png', 'assets/mainChar/walkUp/mainChar_walkUp.json');
+		game.load.atlasJSONHash('bot', 'assets/mC_wU.png', 'assets/mC_wU.json');
     } 
 
     ///Use to instantiate objects before the game starts
@@ -25,11 +33,13 @@ var bot;
         this.controlManager = new ControlManager();
 
         game.world.setBounds(0, 0, gameWidth, totalHeight);
-        this.player = new Player();
-	    game.physics.enable(this.player.sprite, Phaser.Physics.ARCADE);
-		game.camera.follow(this.player.sprite);
+        game.physics.startSystem(Phaser.Physics.P2JS);
 
-        this.cursors = game.input.keyboard.createCursorKeys();
+        this.player = new Player();
+        game.physics.p2.enable(this.player.sprite);
+        game.camera.follow(this.player.sprite);
+
+        cursors = game.input.keyboard.createCursorKeys();
 
 	    //game.camera.deadzone = new Phaser.Rectangle(100, 100, 600, 400)
 	    
@@ -48,20 +58,18 @@ var bot;
 
     ///Called every frame for updating
     function Update() {
-		this.player.vel_x = 0;
-		this.player.vel_y = 0;
-		
         if(this.controlManager.IsArrowKeyUp_Pressed()) {
-            this.player.vel_y += -1;
+            this.player.vel_y = -1;
         }
-		if(this.controlManager.IsArrowKeyDown_Pressed()) {
-            this.player.vel_y += 1;
+        else if(this.controlManager.IsArrowKeyDown_Pressed()) {
+            this.player.vel_y = 1;
         }
+
         if(this.controlManager.IsArrowKeyLeft_Pressed()) {
-            this.player.vel_x += -1;
+            this.player.vel_x = -1;
         }
-		if (this.controlManager.IsArrowKeyRight_Pressed()) {
-            this.player.vel_x += 1;
+        else if (this.controlManager.IsArrowKeyRight_Pressed()) {
+            this.player.vel_x = 1;
         }
         this.player.Update(GAME_SPEED);
     }
@@ -71,6 +79,7 @@ var bot;
 	    game.context.fillStyle = 'rgba(255,0,0,0.6)';
 	    game.context.fillRect(gameWidth, 0, kodingWidth, totalHeight);		
     }
+
 };
 
 Player = function() {
@@ -81,38 +90,25 @@ Player = function() {
     this.sprite.angle = 0;
     this.sprite.rotation = 0;
     this.sprite.anchor.setTo(0.5, 0.5);
-	
+//    this.sprite.immovable = false;
+
     this.Update = function(deltaTime) {
+		
 		// How much you'll move
 		var deltaX = this.vel_x * PLAYER_SPEED * deltaTime;
 		var deltaY = this.vel_y * PLAYER_SPEED * deltaTime;
 	
-		// This is where the player will move to
-		var newX = deltaX + this.sprite.body.x;
-		var newY = deltaY + this.sprite.body.y;
+		// Distance from an edge
+		var distX = Math.min(gameWidth - this.sprite.body.x, this.sprite.body.x);
+		var distY = Math.min(totalHeight - this.sprite.body.y, this.sprite.body.y);
 		
-		// Gets dimensions of sprite
-		var spriteWidth = this.sprite.body.width;
-		var spriteHeight = this.sprite.body.height;
-	
-		// Checks if out of bounds
-		if (newX < 0) {
-			newX = 0;
-		}
-		else if (newX > gameWidth - spriteWidth) {
-			newX = gameWidth - spriteWidth;
-		}
-		
-		if (newY < 0) {
-			newY = 0;
-		}
-		else if (newY > totalHeight - spriteHeight) {
-			newY = totalHeight - spriteHeight;
-		}
-		
-		// Updates Sprite
-        this.sprite.body.x = newX;
-        this.sprite.body.y = newY;
+        this.sprite.body.x += Math.min(deltaX, distX);
+        this.sprite.body.y += Math.min(deltaY, distY);
+
+		if (this.sprite.body.x < 0)
+			this.sprite.body.x = 0;
+		if (this.sprite.body.y < 0)
+			this.sprite.body.y = 0;
 
         this.vel_x = 0;
         this.vel_y = 0;
